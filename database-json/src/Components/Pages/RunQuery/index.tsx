9202578df2a485248703c47cf9943b6d2f3c360b
showkot <shossain@nd.edu>
2025-09-02 09:23:14 -0400

    default query change

--- database-json/src/Components/Pages/RunQuery/index.tsx
+++ database-json/src/Components/Pages/RunQuery/index.tsx
@@ -59,7 +59,7 @@ const RunQuery: React.FC = () => {
   
   const { isConnected, account } = useWeb3();
   
-  const [inputQuery, setInputQuery] = useState<string>("SELECT * FROM patient_data WHERE PatientID = '99'");
+  const [inputQuery, setInputQuery] = useState<string>("SELECT * FROM patient_data WHERE PatientID = '323'");
   const [indexAttribute, setIndexAttribute] = useState<string>("PatientID");
   const [isWalletModalOpen, setIsWalletModalOpen] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
@@ -131,7 +131,7 @@ const RunQuery: React.FC = () => {
   const exampleQueries = [
     {
       label: "Get Patient by ID",
-      query: "SELECT * FROM patient_data WHERE PatientID = '99'",
+      query: "SELECT * FROM patient_data WHERE PatientID = '323'",
       description: "Retrieve specific patient data"
     },
     {
